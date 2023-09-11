e9cf4fe416e7dc621a50c87163f226f1f920f566
jsuazo <javier2587@gmail.com>
2023-09-11 16:13:07 -0300

    Commit WIN_CLOSE

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Text')],
+    [sg.Text('Text'), sg.Spin(['Item 1', 'Item 2'])],
     [sg.Button('Button')],
-    [sg.Input()]
+    [sg.Input()],
+    [sg.Text('Test'), sg.Button('Test Button')]
 ]
 
-sg.Window('Converter', layout).read()
+window = sg.Window('Converter', layout)
 
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
