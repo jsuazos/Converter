a208ece91d750d7b5b76839143c484254006068d
jsuazo <javier2587@gmail.com>
2023-09-11 16:20:22 -0300

    event key

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import PySimpleGUI as sg
 
 layout = [
     [sg.Text('Text'), sg.Spin(['Item 1', 'Item 2'])],
-    [sg.Button('Button')],
+    [sg.Button('Button', key = 'Button1')],
     [sg.Input()],
-    [sg.Text('Test'), sg.Button('Test Button')]
+    [sg.Text('Test'), sg.Button('Test Button', key = 'Button2')]
 ]
 
 window = sg.Window('Converter', layout)
@@ -14,5 +14,10 @@ while True:
 
     if event == sg.WIN_CLOSED:
         break
+    if event == 'Button1':
+        print('button pressed')
+    if event == 'Button2':
+        print('something else')
+
 
 window.close()
